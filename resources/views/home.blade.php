bf19de45eade07544a052609160226fa1c2f3b16
farkhan <farhanhamzah71@webmail.umm.ac.id>
2021-04-27 21:42:52 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.app')
+@section('title', 'Chill Zone Page')
+@section('maincontent')
+
+    <h1>This is home page</h1>
+
+@endsection
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,5 @@
+<ul>
+    <li>this is item one</li>
+    <li>this is item two</li>
+    <li>this is item three</li>
+</ul>
