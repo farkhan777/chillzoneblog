eb8b24c7fcbf36c8ecce785e7479a3a5e8a288cb
farkhan <farhanhamzah71@webmail.umm.ac.id>
2021-04-28 09:45:22 +0700

    add logo

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -6,9 +6,9 @@
 						<div class="row align-items-center">
 							<div class="col-4 col-md-4 col-lg-4">
 								<div class="logo">
-									<a href="home.html">
+									<a href="/">
 										<div class="logo_img">
-											<img src="https://info.appifylab.com/img/appifylab_logo.png" alt="image">
+											<img src="https://scontent.fsub8-1.fna.fbcdn.net/v/t1.6435-9/178400701_521250482378042_5204564419246067314_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=e3f864&_nc_ohc=HBGk95FDsf0AX-S5Iko&_nc_ht=scontent.fsub8-1.fna&oh=6a81cd084631041d2db0b321566d2bb0&oe=60AEF817" alt="image">
 										</div>
 									</a>
 								</div>
@@ -18,7 +18,7 @@
 									<div class="menu_right_list">
 										<ul class="menu_right_ul d-flex">
 											<li class="dis_fx_cntr">
-												<a href="home.html">HOME</a>
+												<a href="/">HOME</a>
 											</li>
 											<li>
 												<a href="gallery.html">gallery</a>
